6ce282e69bac60703b71d9cf968b661bbf30eeb8
RatofWrath <T_mullen93@yahoo.com>
2019-07-19 21:22:48 -0500

    added a create armor function and three armors

--- Armor/armors.js
+++ Armor/armors.js
@@ -1,1 +1,5 @@
 var createArmor = require("./createArmor.js")
+
+var leatherArmor = new createArmor.createArmor(0,2,true,true,false,true,true);
+var chainMail = new createArmor.createArmor(1,4,true,false,false,true,true);
+var plateMail = new createArmor.createArmor(3,6,true,false,false,true,false);
--- Armor/createArmor.js
+++ Armor/createArmor.js
@@ -1,5 +1,12 @@
 module.exports = {
-    createArmor: function (){
-        
+    createArmor: function (minDefense, maxDefense, usableByWarrior, usableByThief, 
+        usableByMage, usableByPaladin, usableByBard){
+        this.minDefense = minDefense;
+        this.maxDefense = maxDefense;
+        this.usableByWarrior = usableByWarrior;
+        this.usableByThief = usableByThief;
+        this.usableByMage = usableByMage;
+        this.usableByPaladin = usableByPaladin;
+        this.usableByBard = usableByBard;
     }
 }
